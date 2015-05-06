144672b967f9faf8ea8713e2d389cd732e513bee
grim_coder <taraskovtun@gmail.com>
2015-05-05 19:09:44 -0700

    initial commit web api for visual studio

---
+++ webapis/ProduceMarketWebApi/ProduceMarketWebApi/Models/Price.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProduceMarketWebApi.Models
+{
+	public class ItemPrice
+	{
+	    public int Id { get; set; }
+
+	    public double Price { get; set; }
+
+	    public string ItemName { get; set; }
+
+	}
+}
