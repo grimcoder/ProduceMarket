b518ea5f67c659caa972b792454add5f06c1069d
grimcoder <papochka12>
2015-07-16 23:30:21 -0700

    cleaning and optimization of the code

--- webapis/ProduceMarketNodeApi/bin/www2.js
+++ webapis/ProduceMarketNodeApi/bin/www2.js
@@ -1,15 +1,19 @@
+/**
+ * Created by taraskovtun on 7/16/15.
+ */
+var express = require('express'),
+    mongoskin = require('mongoskin'),
+    bodyParser = require('body-parser'),
+    logger = require('morgan');
+
 var path = require('path');
 var fs = require('fs');
 
 var saveToFile = function (obj, filenam) {
-
-
     fs.writeFile(path.join(__dirname, '../data/', filenam), JSON.stringify(obj), function (err) {
-
         if (err) {
             return console.log(err);
         }
-
         console.log("The file was saved!");
     });
 };
@@ -21,18 +25,27 @@ var readFromFile = function (filenam) {
 
 };
 
-
-var express = require('express');
-
-var app = express();
-
-var router = express.Router();
-
 var prices = readFromFile("prices");
 var sales = readFromFile("sales");
 var priceChanges = readFromFile("priceChanges");
 
-router.get('/prices', function (req, res, next) {
+var app = express();
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.use(logger());
+
+
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../app')));
+app.use(express.static(path.join(__dirname, '../bower_components')));
+
+
+app.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+app.get('/api/prices', function (req, res) {
     if (req.query.id) {
         res.json(
             prices.filter(
@@ -46,37 +59,13 @@ router.get('/prices', function (req, res, next) {
     }
 });
 
-// DELETE /prices?id=xxx
+app.post('/api/prices', function (req, res) {
 
-router.handlePriceDelete = function (req, res) {
-
-    var id = req.query.id;
-    var priceToDelete = prices.filter(function (i) {
-        return i.Id == id;
-    })[0];
-
-    priceToDelete.Action = 'Delete';
-    priceChanges.push(priceToDelete);
-
-
-    prices = prices.filter(function (i) {
-        return i.Id != id;
-    });
-    saveToFile(prices, "prices.json");
-    saveToFile(priceChanges, "priceChanges.json");
-
-    res.sendStatus(200);
-};
-
-
-// POST /prices/
-router.handlePricePost = function (req, res) {
     var data = req.body;
     var Action;
     var priceWas;
     if (data.Id) {
         Action = "Edit";
-
         priceWas = prices.filter(
             function (i) {
                 return i.Id == data.Id;
@@ -96,7 +85,6 @@ router.handlePricePost = function (req, res) {
             .reduce(function (previousValue, currentValue, index, array) {
                 return previousValue < currentValue ? currentValue : previousValue;
             }) + 1;
-
         data.Id = maxId;
     }
 
@@ -108,45 +96,29 @@ router.handlePricePost = function (req, res) {
     saveToFile(priceChanges, "priceChanges.json");
     res.sendStatus(200);
 
-};
-
-router.get('/sales', function (req, res, next) {
-    if (req.query.id) {
-
-        res.json(sales.filter(function (i) {
-            return i.Id == req.query.id;
-        }));
-    }
-
-    else {
-
-        res.json(sales);
-
-    }
 });
 
+app.delete('/api/prices', function (req, res) {
 
-router.get('/incomes', function (req, res, next) {
-    if (req.query.id) {
-        res.json(incomes.filter(function (i) {
-            return i.Id == req.query.id;
-        }));
-    }
+    var id = req.query.id;
+    var priceToDelete = prices.filter(function (i) {
+        return i.Id == id;
+    })[0];
 
-    else {
+    priceToDelete.Action = 'Delete';
+    priceChanges.push(priceToDelete);
 
-        res.json(incomes);
 
-    }
-});
-
-router.get('/reports/prices',
-    function (req, res) {
-        res.json(priceChanges);
+    prices = prices.filter(function (i) {
+        return i.Id != id;
     });
+    saveToFile(prices, "prices.json");
+    saveToFile(priceChanges, "priceChanges.json");
 
+    res.sendStatus(200);
+});
 
-router.handleSaleDelete = function (req, res) {
+app.delete('/api/sales', function (req, res) {
 
     var id = req.query.id;
     sales = sales.filter(function (i) {
@@ -155,10 +127,9 @@ router.handleSaleDelete = function (req, res) {
     saveToFile(sales, "sales.json");
     res.sendStatus(200);
 
-};
+});
 
-
-router.handleSaleUpdate = function (req, res) {
+app.post('/api/sales', function (req, res) {
 
     var sale = req.body;
     if (!sale.Id) {
@@ -181,7 +152,29 @@ router.handleSaleUpdate = function (req, res) {
     saveToFile(sales, "sales.json");
     res.sendStatus(200);
 
-};
+});
+
+app.get('/api/sales', function (req, res) {
+    if (req.query.id) {
+
+        res.json(sales.filter(function (i) {
+            return i.Id == req.query.id;
+        }));
+    }
+
+    else {
+
+        res.json(sales);
+
+    }
+});
+
+app.get('/api/reports/prices', function (req, res) {
+    res.json(priceChanges);
+});
 
 
-module.exports = router;
+app.listen(3000, function () {
+    console.log('Server is running')
+});
+
