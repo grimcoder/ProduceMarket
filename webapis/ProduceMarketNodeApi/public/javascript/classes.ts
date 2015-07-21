5f314c1bbd5060c81ab2d0560c0124e7b775e93a
taraskovtun <taraskovtun@gmail.com>
2015-07-21 09:08:44 -0700

    started implementing typescript models and moved angular files to common location

---
+++ webapis/ProduceMarketNodeApi/public/javascript/classes.ts
@@ -0,0 +1,27 @@
+/**
+ * Created by taraskovtun on 7/21/15.
+ */
+module ProduceMarket {
+    export class Price {
+        Id:number;
+        Price:number;
+        ItemName:string;
+
+        constructor(id:number, price:number, itemname:string) {
+            this.Id = id;
+            this.ItemName = itemname;
+            this.Price = price
+        }
+    }
+    export class Price {
+        Id:number;
+        Price:number;
+        ItemName:string;
+
+        constructor(id:number, price:number, itemname:string) {
+            this.Id = id;
+            this.ItemName = itemname;
+            this.Price = price
+        }
+    }
+}
